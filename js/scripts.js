0e6ea918b8cae5a6e07c8c0546fb0ea2ad18d991
Ryan Dikeos <ryan.dikeos@gmail.com>
2019-01-25 11:40:50 -0800

    add function between rangeFinder and  change, output is a range of zero though so it's still broken

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
-var raguu = function(result) {
+var rangeFinder = function(result) {
   var range = [];
   for (i=0; i<=result; i++) {
-    range.push(i)
+    range.push(i);
   }
-    return range
+    return range;
 }
 
 var change = function(result) {
   for (i=0; i<=result; i++) {
     if (result === 1) {
-      return "Beep!"
+      return "Beep!";
     } else if (result === 2) {
-      return "Boop!"
+      return "Boop!";
     } else if (result === 3) {
-      return "I'm sorry, Dave. I'm afraid I can't do that."
+      return "I'm sorry, Dave. I'm afraid I can't do that.";
     } else {
-      return result
+      return result;
     }
   }
 }
@@ -26,9 +26,9 @@ $(document).ready(function() {
   $(".form").submit(function(event) {
     event.preventDefault();
     var input = $("input#input").val();
-    var number = (parseInt(input))
-    var range = raguu(number);
-    var results = change(range)
-    $("#results").text(range);
+    var range = rangeFinder(input);
+    var number = (parseInt(range));
+    var results = change(number);
+    $("#results").text(results);
   });
 });
