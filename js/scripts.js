f41fe85906d9eb0b91e28f2aca75a12ab9d10950
Ryan Dikeos <ryan.dikeos@gmail.com>
2019-01-25 09:07:43 -0800

    fixed if else statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
-function change(result) {
-  if (result = 1) {
+var change = function(result) {
+  for (i=0; i<=result; i++)
+// function change(result) {
+  if (result === 1) {
     return "Beep!"
-  } else if (result = 2) {
+  } else if (result === 2) {
     return "Boop!"
-  } else if (result = 3) {
+  } else if (result === 3) {
     return "I'm sorry, Dave. I'm afraid I can't do that."
   } else {
     return result
   }
 }
 
-// var change = function(result) {
-//   for (i=0; i<=result; i++)
-//     if (result = 1)
-// }
+
 
 $(document).ready(function() {
   $(".form").submit(function(event) {
     event.preventDefault();
-    var result = $("input#input").val();
+    var input = $("input#input").val();
+    var result = (parseInt(input))
     var results = change(result)
     $("#results").text(results);
   });
