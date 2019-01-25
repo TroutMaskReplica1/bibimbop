067393476d2784eec59a9dc5677e5b20d4c53bc0
Ryan Dikeos <ryan.dikeos@gmail.com>
2019-01-25 15:55:14 -0800

    restructure if else to meet all specs

--- js/scripts.js
+++ js/scripts.js
@@ -11,12 +11,12 @@ function change(result) {
   for (i=0; i<result.length; i++) {
     var string = result[i].toString();
     var split = string.split("");
-    if (split.includes("1")) {
-      array.push("Beep!");
+    if (split.includes("3")) {
+    array.push(" I'm sorry, Dave. I'm afraid I can't do that. ");
     } else if (split.includes("2")) {
-      array.push("Boop!");
-    } else if (split.includes("3")) {
-      array.push(" I'm sorry, Dave. I'm afraid I can't do that. ");
+      array.push(" Boop! ");
+    } else if (split.includes("1")) {
+      array.push(" Beep! ");
     } else {
       array.push(" " + result[i] + " ");
     }
