ff33c6a3b52bde97a1a442f7bcb7beb692330888
Ryan Dikeos <ryan.dikeos@gmail.com>
2019-01-25 15:19:16 -0800

    add minor edits, trying differnt suggestions from stackoverflow

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,37 @@
-var rangeFinder = function(result) {
+function rangeFinder(result) {
   var range = [];
-  for (i=0; i<=result; i++) {
+  for (i=0; i<=result.length; i++) {
     range.push(i);
   }
     return range;
 }
 
-var change = function(result) {
-  for (i=0; i<=result; i++) {
-    if (result === 1 && result !== 2 && result !== 3) {
-      return "Beep!";
-    } else if (result === 2 && result !== 3) {
-      return "Boop!";
-    } else if (result === 3) {
-      return "I'm sorry, Dave. I'm afraid I can't do that.";
+function change(result) {
+  var array = [];
+  for (i=0; i<result.length; i++) {
+    var string = result[i].toString();
+    var split = string.split("");
+    if (split.includes("1")) {
+      array.push("Beep!");
+    } else if (split.includes("2")) {
+      array.push("Boop!");
+    } else if (split.includes("3")) {
+      array.push("I'm sorry, Dave. I'm afraid I can't do that.");
     } else {
-      return result;
+      array.push(alert("nope"));
     }
   }
+  alert(array)
+  return array;
 }
-
-
 
 $(document).ready(function() {
   $(".form").submit(function(event) {
-    event.preventDefault();
     var input = $("input#input").val();
     var range = rangeFinder(input);
-    var number = (parseInt(range));
-    var results = change(number);
-    $("#results").text(results);
+    var results = change(range);
+    var output = (parseInt(results));
+    $("#results").text(output);
+    event.preventDefault();
   });
 });
