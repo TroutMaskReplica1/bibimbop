66227aec69d3ed5af08710b0280bdee314fc0aa0
Ryan Dikeos <ryan.dikeos@gmail.com>
2019-01-25 12:04:47 -0800

    clean up UI logic, get data manipulation outta there

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,9 @@ var rangeFinder = function(result) {
 
 var change = function(result) {
   for (i=0; i<=result; i++) {
-    if (result === 1) {
+    if (result === 1 && result !== 2 && result !== 3) {
       return "Beep!";
-    } else if (result === 2) {
+    } else if (result === 2 && result !== 3) {
       return "Boop!";
     } else if (result === 3) {
       return "I'm sorry, Dave. I'm afraid I can't do that.";
@@ -20,15 +20,10 @@ var change = function(result) {
   }
 }
 
-
-
 $(document).ready(function() {
   $(".form").submit(function(event) {
     event.preventDefault();
-    var input = $("input#input").val();
-    var range = rangeFinder(input);
-    var number = (parseInt(range));
-    var results = change(number);
-    $("#results").text(results);
+    var input = rangeFinder(change(parseInt($("input#input").val())));
+    $("#results").text(input);
   });
 });
